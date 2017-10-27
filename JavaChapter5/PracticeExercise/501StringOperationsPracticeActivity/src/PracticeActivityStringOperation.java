50fcb248a02d70ca67aa4b2ef1c2da7f6d330211
ForgedGamer <ForgedGamer@github.com>
2017-10-27 14:29:03 -0700

    s

--- JavaChapter5/PracticeExercise/501StringOperationsPracticeActivity/src/PracticeActivityStringOperation.java
+++ JavaChapter5/PracticeExercise/501StringOperationsPracticeActivity/src/PracticeActivityStringOperation.java
@@ -1,4 +1,16 @@
 
-public class PracticeActivityStringOperation {
+public class PracticeActivityStringOperation 
+{
+	public static void main(String args[])
+	{
+		String Name = "Evan Darnell";
+		int len= Name.length();
+		System.out.println("The amount of characters in "+ Name+ " is " + Name.length());
+		for (int i =len -1; i >= 0; i--)
+		{
+		System.out.print(Name.charAt(i));
+		}
 
+
+	}
 }
